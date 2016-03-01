b84a3f262d03a234a91e4ae6455b1a6f45c12e49
thermatk <thermatk@thermatk.com>
2016-03-01 17:58:01 +0100

    Ongoing work

--- app/src/main/java/com/thermatk/android/meatb/InitData.java
+++ app/src/main/java/com/thermatk/android/meatb/InitData.java
@@ -7,12 +7,96 @@ public class InitData extends RealmObject {
     @Required
     private String firstname;
     private String lastname;
+
+
     private String photo_url;
     private String carreerTitle;
     private String carreerDescription;
-    private int carreerId;
+    private String carreerNotes;
+    private long carreerId;
     private String careerDateStart;
 
-    private int lastUpdate;
+    private long lastUpdated;
     private String rawData;
+
+
+    public String getCarreerNotes() {
+        return carreerNotes;
+    }
+
+    public void setCarreerNotes(String carreerNotes) {
+        this.carreerNotes = carreerNotes;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhoto_url() {
+        return photo_url;
+    }
+
+    public void setPhoto_url(String photo_url) {
+        this.photo_url = photo_url;
+    }
+
+    public String getCarreerTitle() {
+        return carreerTitle;
+    }
+
+    public void setCarreerTitle(String carreerTitle) {
+        this.carreerTitle = carreerTitle;
+    }
+
+    public String getCarreerDescription() {
+        return carreerDescription;
+    }
+
+    public void setCarreerDescription(String carreerDescription) {
+        this.carreerDescription = carreerDescription;
+    }
+
+    public long getCarreerId() {
+        return carreerId;
+    }
+
+    public void setCarreerId(long carreerId) {
+        this.carreerId = carreerId;
+    }
+
+    public String getCareerDateStart() {
+        return careerDateStart;
+    }
+
+    public void setCareerDateStart(String careerDateStart) {
+        this.careerDateStart = careerDateStart;
+    }
+
+    public long getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(long lastUpdate) {
+        this.lastUpdated = lastUpdate;
+    }
+
+    public String getRawData() {
+        return rawData;
+    }
+
+    public void setRawData(String rawData) {
+        this.rawData = rawData;
+    }
 }
--- app/src/main/java/com/thermatk/android/meatb/com/thermatk/android/meatb/activities/MainActivity.java
+++ app/src/main/java/com/thermatk/android/meatb/com/thermatk/android/meatb/activities/MainActivity.java
@@ -2,12 +2,15 @@ package com.thermatk.android.meatb.com.thermatk.android.meatb.activities;
 
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
@@ -16,8 +19,12 @@ import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileSettingDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerImageLoader;
+import com.squareup.picasso.Picasso;
 import com.thermatk.android.meatb.LogConst;
 import com.thermatk.android.meatb.R;
 
@@ -34,25 +41,29 @@ public class MainActivity extends AppCompatActivity {
             finish();
             startActivity(i);
         }
+
+        //////// NavDrawer
+        drawerStart();
+        //////////
+    }
+    private void drawerStart() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerBuilder resultBuilder= new DrawerBuilder()
                 .withActivity(this)
                 .withToolbar(toolbar);
         ///////
+
+        //picasso Loader
+        picassoDrawerLoader();
         ColorDrawable cd = new ColorDrawable(0xFF42A5F5);
         AccountHeader headerResult = new AccountHeaderBuilder()
                 .withActivity(this)
                 .withHeaderBackground(cd)
                 .addProfiles(
-                        new ProfileDrawerItem().withName("Random name").withEmail("3123232@studbocconi.it")//.withIcon(getResources().getDrawable(R.drawable.profile))
+                        new ProfileDrawerItem().withName("Random name").withEmail("3323141")
                 )
-                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
-                    @Override
-                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
-                        return false;
-                    }
-                })
+                .withSelectionListEnabled(false)
                 .build();
 
         resultBuilder.withAccountHeader(headerResult);
@@ -67,6 +78,23 @@ public class MainActivity extends AppCompatActivity {
                 item2
         );
         Drawer result = resultBuilder.build();
+
+        // TODO: on first time only
         result.openDrawer();
+    }
+
+    private void picassoDrawerLoader() {
+        DrawerImageLoader.init(new AbstractDrawerImageLoader() {
+            @Override
+            public void set(ImageView imageView, Uri uri, Drawable placeholder) {
+                Picasso.with(imageView.getContext()).load(uri).placeholder(placeholder).into(imageView);
+            }
+
+            @Override
+            public void cancel(ImageView imageView) {
+                Picasso.with(imageView.getContext()).cancelRequest(imageView);
+            }
+
+        });
     }
 }

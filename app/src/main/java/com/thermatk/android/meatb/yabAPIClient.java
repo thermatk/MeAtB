c8b934e2754df190e3e049ed5754ba458b6f6e4f
thermatk <thermatk@thermatk.com>
2016-03-01 14:58:04 +0100

    Move login method to class

--- app/src/main/java/com/thermatk/android/meatb/yabAPIClient.java
+++ app/src/main/java/com/thermatk/android/meatb/yabAPIClient.java
@@ -1,8 +1,27 @@
 package com.thermatk.android.meatb;
 
-/**
- * Created by thermatk on 26.02.16.
- */
-public class yabAPIClient {
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.thermatk.android.meatb.com.thermatk.android.meatb.activities.MainActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class yabAPIClient {
+    public static void getLogin(String username, String password, JsonHttpResponseHandler responseHandler) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        String testLoginURL = "http://ks3-mobile.unibocconi.it/universityapp_prod/api/v6/app/init?os=android";
+        client.addHeader("auth_secret", "b0cc0n1s3cr3t");
+        client.setBasicAuth(username, password);
+        client.get(testLoginURL, responseHandler);
+    }
 }

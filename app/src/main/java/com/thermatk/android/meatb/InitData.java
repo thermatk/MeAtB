b70f4592ea2ca883e05f386d54d216f3aa147b7f
thermatk <thermatk@thermatk.com>
2016-03-01 14:56:51 +0100

    data

--- app/src/main/java/com/thermatk/android/meatb/InitData.java
+++ app/src/main/java/com/thermatk/android/meatb/InitData.java
@@ -1,7 +1,18 @@
 package com.thermatk.android.meatb;
 
 import io.realm.RealmObject;
+import io.realm.annotations.Required;
 
 public class InitData extends RealmObject {
-    
+    @Required
+    private String firstname;
+    private String lastname;
+    private String photo_url;
+    private String carreerTitle;
+    private String carreerDescription;
+    private int carreerId;
+    private String careerDateStart;
+
+    private int lastUpdate;
+    private String rawData;
 }

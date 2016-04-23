cf3f52fe3bed79459006dce089a804a45c5fbc4b
thermatk <thermatk@thermatk.com>
2016-04-23 10:51:51 +0200

    Imports clean

--- app/src/main/java/com/thermatk/android/meatb/fragments/ProfileFragment.java
+++ app/src/main/java/com/thermatk/android/meatb/fragments/ProfileFragment.java
@@ -1,45 +1,11 @@
 package com.thermatk.android.meatb.fragments;
 
-import android.content.Context;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.github.tibolte.agendacalendarview.AgendaCalendarView;
-import com.github.tibolte.agendacalendarview.CalendarManager;
-import com.github.tibolte.agendacalendarview.CalendarPickerController;
-import com.github.tibolte.agendacalendarview.models.CalendarEvent;
-import com.github.tibolte.agendacalendarview.models.IDayItem;
-import com.github.tibolte.agendacalendarview.models.IWeekItem;
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.thermatk.android.meatb.LogConst;
 import com.thermatk.android.meatb.R;
-import com.thermatk.android.meatb.agenda.ACVDay;
-import com.thermatk.android.meatb.agenda.ACVWeek;
-import com.thermatk.android.meatb.agenda.BocconiCalendarEvent;
-import com.thermatk.android.meatb.agenda.BocconiEventRenderer;
-import com.thermatk.android.meatb.data.AgendaEvent;
-import com.thermatk.android.meatb.data.DataWriter;
-import com.thermatk.android.meatb.data.EventDay;
-import com.thermatk.android.meatb.yabAPIClient;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-import java.util.Locale;
-
-import cz.msebera.android.httpclient.Header;
-import io.realm.Realm;
-import io.realm.RealmList;
-import io.realm.RealmResults;
 
 
 public class ProfileFragment extends Fragment{

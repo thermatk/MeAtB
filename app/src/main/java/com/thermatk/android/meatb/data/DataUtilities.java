6b4bdf22eec64bab5294e81b0b7d7124e2f6fc0d
thermatk <thermatk@thermatk.com>
2016-04-25 23:58:12 +0200

    temporary downgrade, inbox 75% done

--- app/src/main/java/com/thermatk/android/meatb/data/DataUtilities.java
+++ app/src/main/java/com/thermatk/android/meatb/data/DataUtilities.java
@@ -277,10 +277,10 @@ public class DataUtilities {
         realm.beginTransaction();
 
         ////// clear the realm
-        realm.delete(RDay.class);
-        realm.delete(RCal.class);
-        realm.delete(REvent.class);
-        realm.delete(RWeek.class);
+        realm.clear(RDay.class);
+        realm.clear(RCal.class);
+        realm.clear(REvent.class);
+        realm.clear(RWeek.class);
         ///////
         RCal rCal = realm.createObject(RCal.class);
 

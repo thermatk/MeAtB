b24a65be70d9398ce7f400ecd7401fe7940b27b7
thermatk <thermatk@thermatk.com>
2016-06-12 01:07:48 +0300

    Notification icon placeholder

--- app/src/main/java/com/thermatk/android/meatb/fragments/ProfileFragment.java
+++ app/src/main/java/com/thermatk/android/meatb/fragments/ProfileFragment.java
@@ -1,11 +1,20 @@
 package com.thermatk.android.meatb.fragments;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.thermatk.android.meatb.R;
+import com.thermatk.android.meatb.activities.MainActivity;
 
 
 public class ProfileFragment extends Fragment{
@@ -28,13 +37,32 @@ public class ProfileFragment extends Fragment{
 
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
-        /*Button mSignInButton = (Button) rootView.findViewById(R.id.button);
+        Button mSignInButton = (Button) rootView.findViewById(R.id.button);
         mSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendRequest();
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                PendingIntent pIntent = PendingIntent.getActivity(getActivity(), (int) System.currentTimeMillis(), intent, 0);
+
+                // Build notification
+                // Actions are just fake
+                Notification noti;
+                Notification.Builder notificationBuilder = new Notification.Builder(getActivity())
+                        .setContentTitle("InboxUpdate" + "test@gmail.com")
+                        .setContentText("Yayyaya").setSmallIcon(R.drawable.ic_notify)
+                        .setContentIntent(pIntent);
+                if (Build.VERSION.SDK_INT < 16) {
+                    noti = notificationBuilder.getNotification();
+                } else {
+                    noti = notificationBuilder.build();
+                }
+                NotificationManager notificationManager = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+                // hide the notification after its selected
+                noti.flags |= Notification.FLAG_AUTO_CANCEL;
+
+                notificationManager.notify(0, noti);
             }
-        });*/
+        });
 
         return rootView;
     }

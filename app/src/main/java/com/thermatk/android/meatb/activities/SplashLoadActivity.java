93ed2cdd5ee934ad06e1694da355f65178c811d7
thermatk <thermatk@thermatk.com>
2016-10-30 17:11:20 +0100

    SplashLoad animations

--- app/src/main/java/com/thermatk/android/meatb/activities/SplashLoadActivity.java
+++ app/src/main/java/com/thermatk/android/meatb/activities/SplashLoadActivity.java
@@ -1,19 +1,36 @@
 package com.thermatk.android.meatb.activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Fade;
 import android.util.Log;
+import android.view.Window;
 
 import com.thermatk.android.meatb.LogConst;
 
 public class SplashLoadActivity extends AppCompatActivity {
 
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        boolean isLollipop = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP);
+
+        if (isLollipop) requestWindowFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+
+
         super.onCreate(savedInstanceState);
+
+        
+        if (isLollipop) {
+            getWindow().setExitTransition(new Fade());
+        }
 
         SharedPreferences mSharedPreferences  =  PreferenceManager.getDefaultSharedPreferences(this);
         Intent i;
@@ -24,7 +41,9 @@ public class SplashLoadActivity extends AppCompatActivity {
             i = new Intent(this, MainActivity.class);
         }
         i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-        finish();
-        startActivity(i);
+
+        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(this);
+        ActivityCompat.startActivity(this, i, options.toBundle());
+        ActivityCompat.finishAfterTransition(this);
     }
 }
